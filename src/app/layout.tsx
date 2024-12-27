174db2611b89b761d5be6a22a3bf9f999854d113
Kris Hagel <hagelk@psd401.net>
2024-12-27 15:46:51 -0800

    Refactor layout structure in RootLayout component
    - Wrapped the main and footer sections within a div for improved layout organization.
    - Enhanced the overall structure of the RootLayout component to ensure better readability and maintainability.
    
    These changes contribute to a cleaner and more structured layout in the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,14 +22,16 @@ export default function RootLayout({
       <body className={`${inter.className} bg-background min-h-screen flex flex-col`}>
         <Providers>
           <AuthenticatorWrapper>
-            <main className="container mx-auto p-8 flex-grow">
-              {children}
-            </main>
-            <footer className="container mx-auto p-4 border-t">
-              <div className="flex justify-end">
-                <SignOutButton />
-              </div>
-            </footer>
+            <div>
+              <main className="container mx-auto p-8 flex-grow">
+                {children}
+              </main>
+              <footer className="container mx-auto p-4 border-t">
+                <div className="flex justify-end">
+                  <SignOutButton />
+                </div>
+              </footer>
+            </div>
           </AuthenticatorWrapper>
         </Providers>
       </body>
